c23f42eb13e4465e89d21adf2bb2bf7e854505af
YegorBabarykin <yegor@babarykin.ru>
2015-03-19 18:33:15 +0300

    without dump

--- Yegor/test.c
+++ Yegor/test.c
@@ -20,7 +20,7 @@ int main()
     set_put(s,122222);
     set_put(s,122222);
     set_put(s,122222);
-    set_dump(s);
+    //set_dump(s);
     printf("set has 15:%s\n",(set_has(s,15)==OK)?"true":"false");
     printf("set has 23425:%s\n",(set_has(s,23425)==OK)?"true":"false");
     set_remove(s,123);
@@ -31,15 +31,15 @@ int main()
     set_remove(s,123);
     set_put(s,256256);
     set_put(s,567);
-    set_dump(s);
+    //set_dump(s);
     printf("set has 123:%s\n",(set_has(s,123)==OK)?"true":"false");
     printf("set has 17:%s\n",(set_has(s,17)==OK)?"true":"false");
     printf("set has 2456256:%s\n",(set_has(s,256256)==OK)?"true":"false");
     set_remove(s,256256);
-    set_dump(s);
+    //set_dump(s);
     printf("set has 2456256:%s\n",(set_has(s,256256)==OK)?"true":"false");
     set_remove(s,567);
     printf("set has 567:%s\n",(set_has(s,567)==OK)?"true":"false");
     set_delete(s);
-    getch();
+    //getch();
 }
